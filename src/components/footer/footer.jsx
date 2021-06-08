2798b56bfbf6b69c2b2cb967338462d4e0d83bbe
Andrey Patrakov <calibraphoto@gmail.com>
2021-06-08 12:17:47 +0300

    футер

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,8 +1,43 @@
 import './footer.scss';
+import logo from './logo.png';
+import logoMobile from './logo_mobile.png';
+import logoTablet from './logo_tablet.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
+      <div className="footer__wrapper">
+        <div className="footer__links">
+          <picture>
+            <source srcSet={logoMobile} media="(max-width: 767.2px)" />
+            <source srcSet={logoTablet} media="(max-width: 1023.2px)" />
+            <img src={logo} alt="Лига Банк" />
+          </picture>
+          <nav>
+            <a href="#">Услуги</a>
+            <a href="#">Рассчитать кредит</a>
+            <a href="#">Контакты</a>
+            <a href="#">Задать вопрос</a>
+          </nav>
+          <p>150015, г. Москва, ул. Московская, д. 32 Генеральная лицензия Банка России №1050 Ⓒ Лига Банк, 2019</p>
+        </div>
+        <div className="footer__contacts">
+          <div className="footer__contact">
+            <a href="tel:*0904">*0904</a>
+            <p>Бесплатно для абонентов МТС, Билайн, Мегафон, Теле2</p>
+          </div>
+          <div className="footer__contact">
+            <a href="tel:88001112233">8 800 111 22 33</a>
+            <p>Бесплатный для всех городов России</p>
+          </div>
+          <div className="footer__social">
+            <a href="#" className="footer__social-link footer__social-link--facebook"></a>
+            <a href="#" className="footer__social-link footer__social-link--instagramm"></a>
+            <a href="#" className="footer__social-link footer__social-link--twitter"></a>
+            <a href="#" className="footer__social-link footer__social-link--youtube"></a>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -334,6 +334,12 @@ const Main = () => {
           <button type="submit">Отправить</button>
         </form>
       </section>
+      <section className="main__offices">
+        <div className="main__offices-map">
+          <h2>Отделения Лига Банка</h2>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d577325.3464971667!2d36.82513809290739!3d55.58152447403445!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54afc73d4b0c9%3A0x3d44d6cc5757cf4c!2z0JzQvtGB0LrQstCw!5e0!3m2!1sru!2sru!4v1623077063555!5m2!1sru!2sru" style={{border: `0`}} allowFullScreen="" loading="lazy"></iframe>
+        </div>
+      </section>
     </main>
   );
 };
